f8b5ec2e9b90ba97fce445e3ec01384a66dd7692
wxwind <373903632@qq.com>
2022-01-20 18:12:41 +0800

    final patch

--- Assets/Restart.cs
+++ Assets/Restart.cs
@@ -10,4 +10,9 @@ public class Restart : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1;
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
